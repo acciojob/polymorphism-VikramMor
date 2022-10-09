daa192a602aff265fd993e20f89e4c1c242e202c
VikramMor <er.vickymor@gmail.com>
2022-10-09 21:18:06 +0530

    product class created inside the main class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,25 +1,28 @@
 package com.driver;
 
 public class Main {
+
+    static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+
+    }
     public static void main(String args[]){
+
         Product p = new Product();
         int resP = p.product(0,1);
         int resP1 = p.product(0,1,2);
         double resP2 = p.product(1.0,2.0);
+        System.out.println(resP + " " +resP1 + " "+resP2);
     }
 }
 
-class Product{
-    public int product(int x, int y) {
-        return x*y;
-    }
-
-    public int product(int x, int y, int z) {
-        return x*y*z;
-    }
-
-    public double product(double x, double y) {
-        return x*y;
-    }
-
-}
